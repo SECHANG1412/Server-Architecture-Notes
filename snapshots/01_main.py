a24a5df5160c16413e9001100facdc8821a6f221
SECHANG1412 <binaryblitz1412@gmail.com>
2026-01-19 09:17:03 +0900

    02_main.py

--- snapshots/01_main.py
+++ snapshots/01_main.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
 
+
+# FastAPI 애플리케이션 객체 생성
 app = FastAPI()
 
+
+# 루트 경로(/)를 처리하는 비동기 함수
 async def read_root():
+    # JSON 응답으로 변환될 데이터 반환
     return {
         "message":"Hello World"
     }
