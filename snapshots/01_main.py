cd8b28168bf59af11714685f1e146961cb86d4a3
SECHANG1412 <binaryblitz1412@gmail.com>
2026-01-19 09:14:03 +0900

    01_main.py

--- snapshots/01_main.py
+++ snapshots/01_main.py
@@ -3,4 +3,6 @@ from fastapi import FastAPI
 app = FastAPI()
 
 async def read_root():
-    return {"message":"Hello World"}
+    return {
+        "message":"Hello World"
+    }
--- snapshots/02_main.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-async def read_root():
-    return {
-        "message":"Hello World"
-    }
